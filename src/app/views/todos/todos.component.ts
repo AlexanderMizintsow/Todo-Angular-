d0d8821e3ad21cd8c9160998b7d5d3e7aff050b9
AlexanderMizintsow <Mizintsow@yandex.ru>
2022-11-12 14:15:36 +0300

    Added sort table -MatSort-

--- src/app/views/todos/todos.component.ts
+++ src/app/views/todos/todos.component.ts
@@ -4,6 +4,7 @@ import {Task} from "../../model/Task";
 import {AfterViewInit, ViewChild} from '@angular/core';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatTableDataSource} from '@angular/material/table';
+import {MatSort} from "@angular/material/sort";
 
 
 @Component({
@@ -11,15 +12,15 @@ import {MatTableDataSource} from '@angular/material/table';
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.scss']
 })
-export class TodosComponent implements OnInit {
+export class TodosComponent implements OnInit, AfterViewInit{
 
   displayedColumns: string[] = ['color', 'position', 'title', 'categories', 'priority', 'date', 'status'];
   dataSource = new MatTableDataSource<Task>;
+
   @ViewChild(MatPaginator) paginator: MatPaginator | undefined;
-  ngAfterViewInit() {
-    // @ts-ignore
-    this.dataSource.paginator = this.paginator;
-  }
+  @ViewChild(MatSort) sort: MatSort | undefined
+  // @ViewChild(MatSort, {static: false}) private sort: MatSort | undefined;
+
   tasks: Task[] | undefined;
 
   constructor(private dataHandlerService: DataHandlerService) {
@@ -33,10 +34,8 @@ export class TodosComponent implements OnInit {
     this.refreshTable();
   }
 
-  private refreshTable() {
-// @ts-ignore
-    this.dataSource.data = this.tasks;
-
+  ngAfterViewInit(): void {
+    this.addTableObjects();
   }
 
   toggleTaskCompleted(task: Task) {
@@ -44,13 +43,57 @@ export class TodosComponent implements OnInit {
   }
 
   // в зависимости от статуса задачи - вернуть цвет названия
-
-
   getPriorityColor(task: Task) {
     if (task.priority && task.priority.color) {
       return task.priority.color;
     }
-
     return '#fff';
   }
+
+
+  // показывает задачи с применением всех текущий условий (категория, поиск, фильтры и пр.)
+  private refreshTable() {
+
+    // @ts-ignore
+    this.dataSource.data = this.tasks; // обновить источник данных (т.к. данные массива tasks обновились)
+
+    this.addTableObjects();
+
+
+    // когда получаем новые данные..
+    // чтобы можно было сортировать по столбцам "категория" и "приоритет", т.к. там не примитивные типы, а объекты
+    // @ts-ignore - показывает ошибку для типа даты, но так работает, т.к. можно возвращать любой тип
+    this.dataSource.sortingDataAccessor = (task, colName) => {
+
+      // по каким полям выполнять сортировку для каждого столбца
+      switch (colName) {
+        case 'priority': {
+          return task.priority ? task.priority.id : null;
+        }
+        case 'categories': {
+          return task.category ? task.category.title : null;
+        }
+        case 'date': {
+          return task.date ? task.date : null;
+        }
+
+        case 'status': {
+          return task.completed ? task.completed : null;
+        }
+
+        case 'title': {
+          return task.title;
+        }
+      }
+    };
+
+  }
+
+  private addTableObjects() {
+    // @ts-ignore
+    this.dataSource.sort = this.sort; // компонент для сортировки данных (если необходимо)
+    // @ts-ignore
+    this.dataSource.paginator = this.paginator; // обновить компонент постраничности (кол-во записей, страниц)
+  }
+
 }
